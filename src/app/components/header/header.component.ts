552424b8fdf2ac8f4b7a25f405f4e717fee6cd35
Rafael Ulisses <ruds.devel@gmail.com>
2024-03-22 08:53:47 -0300

    Criação componentes

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatDialogModule,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  handleOpenModal() {
+    alert('Adicionar tarefa')
+  }
+}
